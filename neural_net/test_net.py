2d8c493052b1e66f0988028ccf3bd260a0ac1e44
ac_kawasoe <kawayan6230@gmail.com>
2023-06-07 16:16:25 +0900

    0607 16:16

--- neural_net/test_net.py
+++ neural_net/test_net.py
@@ -17,9 +17,10 @@ from net_batch_drop_weight.multi_layer_extend import MultiLayerNetExtend
 train_size = x_train.shape[0]
 
 search_hyper_param = 16
-check_score = 0.8
+check_score = 0.85
+num_score = 100
 
-train_iteration_num = 400
+train_iteration_num = 500
 batch_size = 100
 
 # weight_decay_lambda = 0.01
@@ -29,7 +30,7 @@ batch_size = 100
 activation = 'sigmoid'
 #weight_init_std='relu'
 weight_init_std='sigmoid'
-hidden_size_list=[100]
+hidden_size_list=[100,50,30]
 #optimizer = Adam(lr=lr)
 optimizer = SGD()
 use_batchnorm=True
@@ -66,8 +67,10 @@ graph_text = (
     dropout_ration
 )
 
+param_list = []
 
-for k in range(30):
+k = 0
+while k < num_score:
     graph_param_train_loss = []
     graph_param_train_accuracy = []
     graph_wd = []
@@ -75,8 +78,9 @@ for k in range(30):
     graph_score = []
     for j in range(search_hyper_param):
         weight_decay = 10 ** np.random.uniform(-8, -5)
-        lr = np.random.uniform(0.04, 0.09)
-        print(str(j),"weight_decay: ",weight_decay," lr: ",lr)
+        #0.04-0.08 0.006-0.01 0.01-0.03
+        lr = np.random.uniform(0.1, 0.0001)
+        #print(str(j),"weight_decay: ",weight_decay," lr: ",lr)
         graph_wd.append(weight_decay)
         graph_lr.append(lr)
         network = MultiLayerNetExtend(input_size=input_size, hidden_size_list=hidden_size_list, output_size=output_size, 
@@ -103,44 +107,56 @@ for k in range(30):
                 graph_param_train_accuracy[j].append(accuracy)
                 if graph_score[j] < accuracy:
                     graph_score[j] = accuracy
+
+            if i == (train_iteration_num-1):
+                if graph_score[j] > check_score:
+                    print("score:"+str(graph_score[j]),"weight_decay: ",weight_decay," lr: ",lr)
+                    param_list.append("score:"+str(graph_score[j])+"weight_decay: "+str(weight_decay)+" lr: "+str(lr))
+                    k+=1
+                #else:
+                    #print(str(k)+"under 0.8")
+            
+        print("score:"+str(graph_score[j]))
+print(param_list)
         #
-    if any(number >= check_score for number in graph_score):
-        print("over "+str(check_score)+str(k))
-        break
-    else:
-        print("not "+ str(check_score) +" over "+str(k))
+    # if any(number >= check_score for number in graph_score):
+    #     print("over "+str(check_score)+str(k))
+    #     break
+    # else:
+    #     print("not "+ str(check_score) +" over "+str(k))
 
+# rows = 4  # 行数
+# cols = int(search_hyper_param/2)  # 列数
+# x1 = np.arange(train_iteration_num)
+# x2_range = train_iteration_num/graph_ar
+# x2 = np.arange(x2_range)
+# # グラフのサイズと間隔を調整
+# fig, axes = plt.subplots(rows, cols, figsize=(8, 6))
+# fig.subplots_adjust(hspace=0.4)
 
-rows = 4  # 行数
-cols = int(search_hyper_param/2)  # 列数
-x1 = np.arange(train_iteration_num)
-x2_range = train_iteration_num/graph_ar
-x2 = np.arange(x2_range)
-# グラフのサイズと間隔を調整
-fig, axes = plt.subplots(rows, cols, figsize=(8, 6))
-fig.subplots_adjust(hspace=0.4)
+# # グラフのプロット
+# for i in range(search_hyper_param):
+#     ax1 = axes[int(np.floor(i/8))*2, np.mod(i,8)]
+#     ax2 = axes[int(np.floor(i/8))*2+1, np.mod(i,8)]
+#     #print(int(np.floor(i/8)), np.mod(i,8))
+#     ax1.plot(x1, graph_param_train_loss[i], 'b-')
+#     ax2.plot(x2, graph_param_train_accuracy[i], 'r-')
 
-# グラフのプロット
-for i in range(search_hyper_param):
-    ax1 = axes[int(np.floor(i/8))*2, np.mod(i,8)]
-    ax2 = axes[int(np.floor(i/8))*2+1, np.mod(i,8)]
-    #print(int(np.floor(i/8)), np.mod(i,8))
-    ax1.plot(x1, graph_param_train_loss[i], 'b-')
-    ax2.plot(x2, graph_param_train_accuracy[i], 'r-')
+#     ax1.set_xlabel(str(np.round(graph_lr[i], decimals=5)))
+#     ax1.set_ylabel(str(graph_score[i]))
+#     ax2.set_xlabel(str(np.round(graph_wd[i], decimals=8)))
+#     ax2.set_ylabel(str(i))
+#     ax1.set_xticks([])
+#     ax1.set_yticks([])
+#     ax2.set_xticks([])
+#     ax2.set_yticks([])
+#     ax1.grid(True)
+#     ax2.grid(True)
 
-    ax1.set_xlabel(str(np.round(graph_lr[i], decimals=5)))
-    ax1.set_ylabel(str(graph_score[i]))
-    ax2.set_xlabel(str(np.round(graph_wd[i], decimals=8)))
-    ax2.set_ylabel(str(i))
-    ax1.set_xticks([])
-    ax1.set_yticks([])
-    ax2.set_xticks([])
-    ax2.set_yticks([])
-    ax1.grid(True)
-    ax2.grid(True)
+# # グラフのタイトルとコメントを設定
+# fig.suptitle(graph_title)
+# #fig.text(0.3, 0.15, graph_text, ha='left', va='center')
 
-# グラフのタイトルとコメントを設定
-fig.suptitle(graph_title)
-#fig.text(0.3, 0.15, graph_text, ha='left', va='center')
+# plt.show()
 
-plt.show()
+#グラフのひょう
