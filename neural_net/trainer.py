835aa5459ef5487ee729b534910790d99d8be660
ac_kawasoe <kawayan6230@gmail.com>
2023-06-09 16:13:41 +0900

    0609 16:13

---
+++ neural_net/trainer.py
@@ -0,0 +1,13 @@
+
+class Trainer:
+
+    """
+        ・network.paramsの更新
+        ・精度のリスト作成
+        ・損失関数のリスト作成
+        ・ハイパーパラメータの更新
+        ・
+    """
+
+    def __init__(self,) -> None:
+        pass
